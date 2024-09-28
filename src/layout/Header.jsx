32390f2cc15f0dc36871e1abbb4f84c1b050c63e
muslmoney <150621917+muslmoney@users.noreply.github.com>
2024-09-28 18:03:37 +0500

    Update Header.jsx

--- src/layout/Header.jsx
+++ src/layout/Header.jsx
@@ -828,26 +828,7 @@ const Header = () => {
                       </svg>
                     </a>
                   </li>
-                  <li className="Header__tools-item">
-                    <a href="/profile" className="Header__tools-link">
-                      <svg
-                        className="Header__tools-profile"
-                        aria-hidden="true"
-                        focusable="false"
-                        viewBox="0 0 24 24"
-                        role="img"
-                        width="24px"
-                        height="24px"
-                        fill="none"
-                      >
-                        <path
-                          stroke="currentColor"
-                          strokeWidth="1.5"
-                          d="M3.75 21v-3a3.75 3.75 0 013.75-3.75h9A3.75 3.75 0 0120.25 18v3M12 3.75a3.75 3.75 0 100 7.5 3.75 3.75 0 000-7.5z"
-                        ></path>
-                      </svg>
-                    </a>
-                  </li>
+            
                   <li className="Header__tools-item">
                     <a
                       href="/bag"
